f93911ec9fd4ffdd49b90f665d5064ec3ff323dc
erwinkarim <erwinkarim@gmail.com>
2013-05-09 01:28:01 +0800

    auto generate code when creating coupon, redeem coupon mech

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -10,5 +10,45 @@ class StoreController < ApplicationController
   #  POST   /store/:persona_id/redeem_coupon(.:format)
   def redeem_coupon
     @coupon_code = params[:redeemCodes]
+
+    #check if the coupon is valid and not taken
+    @coupon = Coupon.where(:code => params[:redeemCodes]).first
+    coupon_failure = false
+    if @coupon.nil? then
+      message = 'Coupon not found'
+      coupon_failure = true
+    elsif !@coupon.redeem_date.nil? then
+      message = 'Coupon has been taken by '+ Persona.find(@coupon.persona_id).first.screen_name
+      coupon_failure = true
+    end
+
+    #if everything ok then update the coupon to be redeem
+    if !coupon_failure then
+      @persona = Persona.where(:screen_name => params[:persona_id]).first
+      @coupon.update_attributes({ :persona_id => @persona.id, :redeem_date => DateTime.now } )
+      @persona.update_attributes( { :premium => true })
+      if @persona.premiumSince.nil? then
+        @persona.update_attributes( { :premiumSince => DateTime.now } )
+      end
+      if @persona.premiumExpire.nil? then
+        @persona.update_attributes( {:premiumExpire => DateTime.now + 1.year })
+      else
+        @persona.update_attributes( { :premiumExpire => @persona.premiumExpire + 1.year })
+      end
+
+      @coupon.save
+      @persona.save
+    end
+
+    if coupon_failure then 
+      respond_to do |format|
+        format.html { redirect_to :back, alert: message }
+      end
+    else
+      respond_to do |format|
+        format.html
+      end
+    end
+
   end
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,14 @@
 class Coupon < ActiveRecord::Base
   belongs_to :persona
-  attr_accessible :code, :expire_date, :redeem_date
+  attr_accessible :code, :expire_date, :redeem_date, :persona_id
+  validates :code, :presence => true, :uniqueness => true
+  before_validation :generate_code
+
+  #generate the codes before validation
+  def generate_code
+    self.code = loop do 
+      random_token = SecureRandom.urlsafe_base64
+      break random_token unless Coupon.where(code: random_token).exists?
+    end
+  end
 end
