babae5329fa9ce2bf9a553810398dfae005ff47d
erwinkarim <erwinkarim@gmail.com>
2013-05-24 01:43:20 +0800

    revamp front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
     @photos = Photo.find(:all, :limit=> 20, :order => 'id desc')
     if persona_signed_in? then
       @persona = current_persona
-      @myPhotos = Photo.find(:all, :limit => 5, :order => 'id desc', 
+      @myPhotos = Photo.find(:all, :limit => 20, :order => 'id desc', 
         :conditions => {:persona_id => current_persona.id})
     end
 
