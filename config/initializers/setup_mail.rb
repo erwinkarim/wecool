b6f5a32e609cf7ee7af2d735699b8cfc01e5c72b
erwinkarim <erwinkarim@gmail.com>
2013-07-17 12:45:29 +0800

    setup sendgrid for heroku

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
-#ActionMailer::Base.smtp_settings = {
-#  :address              => "smtp.gmail.com",
-#  :port                 => 587,
-#  :domain               => "gmail.com",
-#  :user_name            => "erwinkarim",
-#  :password             => "14msup3rm4n",
-#  :authentication       => "plain",
-#  :enable_starttls_auto => true
-#}
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
