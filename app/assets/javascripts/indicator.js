af78a56c68486ba67ffd85e7a72bfdd363dc27e3
erwinkarim <erwinkarim@gmail.com>
2013-07-22 12:20:52 +0800

    issue #25: show loading icon even when empty set (initial loading), fix positional issue when loading photos during mediaset edit page

--- app/assets/javascripts/indicator.js
+++ app/assets/javascripts/indicator.js
@@ -1,11 +1,20 @@
 var indicator = {};
 
 //some spinning picture to indicate the pictures are comming
-//arguments: target => which accordion to put the load in
-//            position => to prepend or append the load icons
-indicator.load = function(target, position){
-	spin = '<div class="loading" style="float:left;"><i class="icon-spinner icon-4x icon-spin"></i></div>';
-	if ( position == 'before') {
+//arguments: target => required. which accordion to put the load in
+//            position => required. to prepend or append the load icons
+//            css_float => optional. css float property. defaults to 'left'
+//            css_display => optional. css display property. defaults to 'block'
+indicator.load = function(target, position, css_float, css_display ){
+  css_float = typeof css_float !== 'undefined' ? css_float : 'left';
+  css_display = typeof css_display !== 'undefined' ? css_display : 'block';
+    
+	spin = '<div class="loading" style="float:'+ css_float + '; display:' + css_display + 
+    '"><i class="icon-spinner icon-4x icon-spin"></i></div>';
+  if ($(target).find('.carousel').length == 0) {
+    //if there's nothing inside, just append inside
+    $(target).append(spin);
+  } else if ( position == 'before') {
 		handle = $(target).find('.carousel:first');
 		handle.before(spin);
 	} else if ( position == 'after') {
