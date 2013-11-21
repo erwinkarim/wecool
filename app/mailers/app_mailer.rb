94471341becb7a876962e8929ca8a42be4d2c882
erwinkarim <erwinkarim@gmail.com>
2013-11-21 14:46:16 +0800

    update from field in email replys

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,7 +1,7 @@
 #class AppMailer < ActionMailer::Base
 class AppMailer < Devise::Mailer
   helper :application
-  default from: "from@example.com"
+  default from: "no-reply@sirap.co"
 
   def test_mail persona
     @persona = persona
@@ -11,8 +11,8 @@ class AppMailer < Devise::Mailer
   end 
 
   def share options={}
-    @default_options = { :from => 'no-reply@wecool.com', :to => 'test@wecool.com',  :subject => 'the subject', 
-      :url => 'http://localhost:3000/test', :persona_sender => nil, :message => nil }
+    @default_options = { :from => 'Sirap <no-reply@sirap.co>', :to => 'devnull@sirap.co',  :subject => 'the subject', 
+      :url => 'http://sirap.co/', :persona_sender => nil, :message => nil }
     @default_options = @default_options.merge options
 
     mail(:to => @default_options[:to], :subject => @default_options[:subject], :from => @default_options[:from] ) do |format|
