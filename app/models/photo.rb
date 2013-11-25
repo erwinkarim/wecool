190c5ebd79bd054b17a3a1db218254576c441263
erwinkarim <erwinkarim@gmail.com>
2013-11-25 17:12:22 +0800

    issue #46: update script to import sets and photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -164,11 +164,7 @@ class Photo < ActiveRecord::Base
       #this is not good, need to get better way of doing it
       # read http://www.michael-noll.com/blog/2013/01/18/implementing-real-time-trending-topics-in-storm/
       test = Array.new
-			if Photo.all.empty? then
-				date_range = 1.week.ago
-			else
-				date_range = Photo.where{ persona_id.in persona_range }.pluck(:created_at).max - 2.weeks
-			end
+      date_range = Photo.where{ persona_id.in persona_range }.pluck(:created_at).max - 2.weeks
       #Photo.where{ (created_at.gt date_range ) & (persona_id.in persona_range) }. order(
       #  'created_at desc').map{ |x| test.empty? ? test << [x] :
       #  (test.last.last.created_at.to_date == x.created_at.to_date ? test.last << x : test << [x]) 
--- db/fixtures/photos_and_sets.rb
+++ db/fixtures/photos_and_sets.rb
@@ -1,2 +1,38 @@
 
 #load seed photos and sets here
+#
+base_dir = Rails.root + 'db/fixtures/photos_and_sets'
+set_id = 0
+file_id = 0
+set_order = 1
+
+Dir.foreach(base_dir) do |entry|
+  next if entry == '.' or entry == '..'
+  Mediaset.seed(:id,
+    #  Mediaset(id: integer, title: string, description: text, persona_id: integer, created_at: datetime, 
+    #   updated_at: datetime, up_votes: integer, down_votes: integer, featured: boolean, system_visible: boolean)
+    { id:set_id, title:entry, description:'Seed data', persona_id:1, featured:true, system_visible:true}
+  )
+  Dir.foreach(base_dir + entry) do |file|
+    next if file == '.' or file == '..'
+        
+    seed_file = File.open(base_dir+entry+file) 
+    Photo.seed(:id,
+      #Photo(id: integer, title: string, description: text, persona_id: integer, created_at: datetime, 
+      # updated_at: datetime, avatar: string, featured: boolean, up_votes: integer, down_votes: integer, 
+      # visible: boolean, system_visible: boolean, 
+      # taken_at: datetime, md5: string, exif: text, width: integer, height: integer)
+      { id:file_id, title:file.sub(/.jpg/, ''), description:'seed photo', persona_id:1, avatar:seed_file,
+        featured:(rand(1..100) > 70), system_visible:true }
+    )
+    MediasetPhoto.seed(:id,
+      #MediasetPhoto(id: integer, photo_id: integer, mediaset_id: integer, order: integer, 
+      # created_at: datetime, updated_at: datetime)
+      { photo_id:file_id, mediaset_id:set_id, order:set_order }
+    )
+    file_id += 1
+    set_order += 1
+  end
+  set_order = 1
+  set_id += 1
+end
