9c80711124d31e015df672f4a70d5afd14a13634
erwinkarim <erwinkarim@gmail.com>
2013-11-28 19:41:34 +0800

    issue #2: update spreedly environment init

--- config/initializers/spreedly.rb
+++ config/initializers/spreedly.rb
@@ -4,5 +4,3 @@ if Rails.env.development? then
   ENV['SPREEDLYCORE_ACCESS_SECRET']='O4lB16v76XS4hVQPIJT4rU7zSas3e4sueE2JHNkQSJUTIovitVYHwb9LuvSG4xMq'
 	ENV['SPREEDLYCORE_GATEWAY_TOKEN']='8vAakAjstrG9k9jeLBhDARNoAic'
 end
-#testing environment for now
-SpreedlyCore.configure
