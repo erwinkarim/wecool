33556524b9f865d0b1d1d5a976a14c0378c31466
erwinkarim <erwinkarim@gmail.com>
2014-01-02 16:08:50 +0800

    update to block only ie for now

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,6 +1,7 @@
 # detech browser configuration
 Rails.configuration.middleware.use Browser::Middleware do
   #TODO: need to allow css and other assets accessible
-  redirect_to browser_unsupported_path unless 
-    browser.firefox? || browser.safari? || browser.opera? || browser.chrome? || browser.mobile? || browser.tablet?
+  if browser.ie? then
+    redirect_to browser_unsupported_path 
+  end
 end
