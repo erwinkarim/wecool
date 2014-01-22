82309ff83bb60778360708b88a53e0ed6b8a3e05
erwinkarim <erwinkarim@gmail.com>
2014-01-22 16:59:32 +0800

    issue #10, update menu for mobile site

--- config/routes.rb
+++ config/routes.rb
@@ -72,7 +72,7 @@ Wecool::Application.routes.draw do
     #match '/photos/download' => :download, :via => :post, :as => 'photo_download'
     match '/photos/:persona_id/dl_test' => :dl_test, :via => :get, :as => 'photos_dl_test'
     match '/photos/:persona_id/get_dups' => :get_dups, :via => :get, :as => 'photos_get_dups'
-    match '/photos/:persona_id/new_direct' => :new_direct, :via => :get, :as => 'new_photo'
+    match '/photos/:persona_id/new' => :new_direct, :via => :get, :as => 'new_photo'
     match '/photos/:persona_id/gen_s3_data' => :gen_s3_data, :via => :get, :as => 'photos_gen_s3_data'
     match '/photos/:persona_id/gen_from_s3' => :gen_from_s3, :via => :post, :as => 'photos_gen_from_s3'
     match '/photos_direct' => :create_direct, :via => :post, :as => 'photos_create_direct'
