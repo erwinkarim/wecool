c438f2d5a54f5865b57baa33858424590e324fbc
erwinkarim <erwinkarim@gmail.com>
2014-02-12 01:19:51 +0800

    issue #73: add auto linking on objects that are on queue. give a better picture what is being editing

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web:	bundle exec unicorn_rails -p $PORT -c ./config/unicorn.rb
-worker: bundle exec rake jobs:work
+#worker: bundle exec rake jobs:work
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
 	include Twitter::Autolink
+
+	def generate_obj_link object
+		if object.instance_of? Photo then
+			return link_to( 'Photo', photo_view_path(Persona.find(object.persona_id).screen_name, object.id))
+		elsif object.instance_of? Persona then
+			return link_to('Persona', persona_path(Persona.where(:screen_name => object.screen_name).first) )
+		else
+			return object
+		end
+	end
 end
